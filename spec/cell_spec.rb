708d30933ae91f654b3ec0145649860552ad056f
David Kennedy <davidkennedy@Davids-MacBook-Air.local>
2025-02-20 15:57:04 -0700

    Write ship and empty methods

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe Cell do
             expect(@cell.coordinate).to eq("B4")
         end
     end
+    describe "checks if ship is on this cell" do
+        it "returns nil" do
+            expect(@cell.ship).to eq(nil)
+        end
+    end
+    describe "checks if the cell is empty" do
+        it "it returns true/false based on if the cell is empty" do
+            expect(@cell.empty?).to be(true)
+        end
+    end
 end
