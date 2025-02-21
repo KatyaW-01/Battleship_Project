d5b4797908e816860119349302a66705af5b36e0
Katya Weicht <katya.weicht@gmail.com>
2025-02-20 20:08:53 -0700

    Write tests for render method

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -47,6 +47,17 @@ RSpec.describe Cell do
             cell_1.fire_upon
 
             expect(cell1.render).to eq("M")
+
+            cell_2 = Cell.new("C3")
+            cruiser = Ship.new("Cruiser", 3)
+
+            cell_2.place_ship(cruiser)
+
+            cell_2.render
+
+            expect(cell_2.render).to eq(".") 
+            
+
         end
     end
 end
